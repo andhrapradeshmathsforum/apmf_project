d542001f29caf9e63d483565ee65c4dfd5112987
suneel <sk0202@gmail.com>
2024-03-09 23:35:15 +0530

    to render

--- apmf_project/settings.py
+++ apmf_project/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'STATICFILES')
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 
 # Default primary key field type
